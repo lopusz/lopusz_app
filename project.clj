8781bcd12741d208e414ae334bde6411a12f54cb
Michal Lopuszynski <lopusz@server.fake>
2014-02-26 16:14:02 +0100

    Fixing description in project.clj and adding README.org.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lopusz_app/lein-template "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Standard project template for lein."
+  :url "http://github.com/lopusz/lopusz_app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
